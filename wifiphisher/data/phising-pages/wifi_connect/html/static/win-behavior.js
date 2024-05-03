9a0bc51df1073a6ebff914535bba1e30fd674ee3
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-04 01:20:24 +0700

    Delete wifiphisher/data/phising-pages directory

--- wifiphisher/data/phising-pages/wifi_connect/html/static/win-behavior.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('connect').onclick = function() {
-    document.getElementsByClassName('disconnected')[0].style.display = 'none';
-    document.getElementsByClassName('connected')[0].style.display = 'block';
-    document.getElementById('password').focus();
-};
